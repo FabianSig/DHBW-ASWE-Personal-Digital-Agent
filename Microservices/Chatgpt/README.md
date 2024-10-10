14e723c98fe7c73b4275d056c1eba1bcb04e1c57
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-10-10 15:32:16 +0200

    Projekt neu angelegt

--- Microservices/Chatgpt/README.md
+++
@@ -1,1 +0,0 @@
-# Personal-Digital-Agent-MS-ChatGPT
--- Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/MsChatGptApplication.java
+++ Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/MsChatGptApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package nikomitk.mschatgpt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MsChatGptApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(MsChatGptApplication.class, args);
     }
 
 }
