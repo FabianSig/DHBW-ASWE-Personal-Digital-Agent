7bad6951d1e91e82400db21ce5ad756e86355ebd
fabiansig <fabian.sigmund1@gmail.com>
2024-10-11 15:58:59 +0200

    test: deploy

--- Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/service/ChatGPTService.java
+++ Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/service/ChatGPTService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class ChatGPTService {
     public static String test(String message) {
-        return "Hello Es wurde automatisch deployed, " + message;
+        return "Hello Es wurde automatisch deployed Test 2, " + message;
     }
 }
