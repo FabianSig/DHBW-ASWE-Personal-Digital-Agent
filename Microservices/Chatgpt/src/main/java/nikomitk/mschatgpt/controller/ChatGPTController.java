c9a5fbcec8c74b6e176043a80322ada3f743f920
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-10-14 10:55:26 +0200

    add: "api key" for this service

--- Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/controller/ChatGPTController.java
+++ Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/controller/ChatGPTController.java
@@ -2,10 +2,7 @@ package nikomitk.mschatgpt.controller;
 
 import lombok.RequiredArgsConstructor;
 import nikomitk.mschatgpt.service.ChatGPTService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chatgpt")
@@ -15,7 +12,10 @@ public class ChatGPTController {
     private final ChatGPTService chatGPTService;
 
     @GetMapping("/test")
-    public String test(@RequestBody String message) {
+    public String test(@RequestBody String message, @RequestHeader String authorization) {
+        if(!authorization.equals(System.getenv("OUR_API_KEY"))) {
+            throw new RuntimeException("Unauthorized");
+        }
         return chatGPTService.test(message);
     }
 }
