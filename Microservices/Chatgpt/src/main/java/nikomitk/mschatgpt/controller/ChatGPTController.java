6810057dd008a08260b04f12c65c7bf7c4d72a04
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-10-14 11:07:53 +0200

    add: fix HttpHeader

--- Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/controller/ChatGPTController.java
+++ Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/controller/ChatGPTController.java
@@ -2,6 +2,8 @@ package nikomitk.mschatgpt.controller;
 
 import lombok.RequiredArgsConstructor;
 import nikomitk.mschatgpt.service.ChatGPTService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,7 +14,8 @@ public class ChatGPTController {
     private final ChatGPTService chatGPTService;
 
     @GetMapping("/test")
-    public String test(@RequestBody String message, @RequestHeader String authorization) {
+    @ResponseStatus(HttpStatus.OK)
+    public String test(@RequestBody String message, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorization) {
         if(!authorization.equals(System.getenv("OUR_API_KEY"))) {
             throw new RuntimeException("Unauthorized");
         }
