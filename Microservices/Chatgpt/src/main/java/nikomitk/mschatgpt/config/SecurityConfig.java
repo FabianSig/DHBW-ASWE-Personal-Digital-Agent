071ca19b7a5938fbae55f67dda2ce8cc6c1b814f
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-10-17 09:49:19 +0200

    fix: disable csrf

--- Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/config/SecurityConfig.java
+++ Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/config/SecurityConfig.java
@@ -1,16 +1,12 @@
 package nikomitk.mschatgpt.config;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import nikomitk.mschatgpt.filter.CustomAPIKeyFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-import java.io.IOException;
 
 @Configuration
 public class SecurityConfig {
@@ -20,6 +16,7 @@ public class SecurityConfig {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
                         .permitAll())
                 .addFilterAfter(new CustomAPIKeyFilter(), BasicAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
 
