bc7aa83da29f1c9b75cc5cb3ddfc760a4e9e1786
Sven Sendke <svensendke@gmail.com>
2024-10-18 12:41:04 +0200

    feat: add menu integration

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -2,19 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SearchBarComponent} from './search-bar/search-bar.component';
 import {ChatGPTResponse} from './interfaces/chat-gptresponse';
+import {MenuComponent} from './menu/menu.component';
+import {MenuResponse} from './interfaces/menu-response';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SearchBarComponent],
+  imports: [RouterOutlet, SearchBarComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Personal-Digital-Agent-Frontend';
   chatGPTResponse?: ChatGPTResponse;
+  menuResponse?: MenuResponse;
 
   onChatGPTResponse(response: ChatGPTResponse): void {
     this.chatGPTResponse = response;
+  }
+
+  onMenuResponse(response: MenuResponse): void {
+    this.menuResponse = response;
   }
 }
--- Frontend/src/app/services/api.service.ts
+++ Frontend/src/app/services/api.service.ts
@@ -6,13 +6,20 @@ import {environment} from '../../environment';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = environment.apiUrl;
+  private apiUrlChatgpt = environment.apiUrlChatgpt;
+  private apiUrlSpeisekarte = environment.apiUrlSpeisekarte;
 
   constructor(private http: HttpClient) {}
 
   getChatGPTData(query: string, key: string) {
     const headers = { 'Authorization': key }
     const body = { message: query };
-    return this.http.post(this.apiUrl, body, {headers} );
+    return this.http.post(this.apiUrlChatgpt, body, {headers} );
+  }
+
+  getMenuData(menuDate: string) {
+    // If no date is given, undefined is used to display the menu for the current day
+    const params = menuDate ? { datum : menuDate } : undefined;
+    return this.http.get(this.apiUrlSpeisekarte, { params });
   }
 }
