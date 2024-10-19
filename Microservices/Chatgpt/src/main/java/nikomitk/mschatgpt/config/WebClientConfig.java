60e7ff3020490fa97c77c5bc2a0d32ff646d4638
fabiansig <fabian.sigmund1@gmail.com>
2024-10-19 13:58:07 +0200

    fix: use WebClient instead of RestClient

--- Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/config/WebClientConfig.java
+++ Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/config/WebClientConfig.java
@@ -5,26 +5,26 @@ import nikomitk.mschatgpt.ChatGPTClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 @Slf4j
-public class RestClientConfig {
+public class WebClientConfig {
 
     @Value("${chatgpt.api.url}")
     private String chatGPTServiceUrl;
 
     @Bean
     public ChatGPTClient chatGPTClient() {
-        RestClient restClient = RestClient.builder()
+        WebClient webClient = WebClient.builder()
                 .baseUrl(chatGPTServiceUrl)
                 .defaultHeader("Authorization", "Bearer " + System.getenv("API_KEY"))
                 .build();
 
-        RestClientAdapter restClientAdapter = RestClientAdapter.create(restClient);
-        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        WebClientAdapter webClientAdapter = WebClientAdapter.create(webClient);
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(webClientAdapter).build();
         return httpServiceProxyFactory.createClient(ChatGPTClient.class);
     }
 }
