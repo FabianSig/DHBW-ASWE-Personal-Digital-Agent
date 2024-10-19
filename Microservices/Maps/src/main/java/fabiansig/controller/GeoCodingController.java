5ae3bea4e8b0f47af4bb1dead4477621d9c34732
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-10-19 16:39:11 +0200

    add: /api to routes

--- Microservices/Maps/src/main/java/fabiansig/controller/GeoCodingController.java
+++ Microservices/Maps/src/main/java/fabiansig/controller/GeoCodingController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/geocoding")
+@RequestMapping("/api/geocoding")
 @RequiredArgsConstructor
 public class GeoCodingController {
 
