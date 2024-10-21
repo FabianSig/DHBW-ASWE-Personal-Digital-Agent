c81ea8c697473d74fdca7bb1d944677d05b62b9b
fabiansig <fabian.sigmund1@gmail.com>
2024-10-21 15:52:40 +0200

    add: new bff api url

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrlChatgpt: 'https://bff.blueflower-541916ee.germanywestcentral.azurecontainerapps.io/api/chatgpt/message',
-  apiUrlSpeisekarte: 'https://bff.blueflower-541916ee.germanywestcentral.azurecontainerapps.io/api/speisekarte',
-  apiUrlAudio: 'https://bff.blueflower-541916ee.germanywestcentral.azurecontainerapps.io/api/chatgpt/audio'
+  apiUrlChatgpt: '/api/chatgpt/message',
+  apiUrlSpeisekarte: '/api/speisekarte',
+  apiUrlAudio: '/api/chatgpt/audio'
 };
