6f8c1e1935052a1005dddf82b1678dadae4079ad
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-10-23 14:15:01 +0200

    fix: apikeyfilter logging

--- Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/filter/CustomAPIKeyFilter.java
+++ Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/filter/CustomAPIKeyFilter.java
@@ -3,12 +3,14 @@ package nikomitk.mschatgpt.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
 /**
  * Custom filter to check the API key
  */
+@Slf4j
 public class CustomAPIKeyFilter implements Filter {
 
     @Override
@@ -16,6 +18,10 @@ public class CustomAPIKeyFilter implements Filter {
 
         // Conversion to HttpServletRequest to access the header
         if(servletRequest instanceof HttpServletRequest httpServletRequest) {
+
+            httpServletRequest.getHeaderNames().asIterator().forEachRemaining(header -> log.info("Header: {}", httpServletRequest.getHeader(header)));
+            log.info("Our api key from env: {}", System.getenv("OUR_API_KEY"));
+
             if(httpServletRequest.getRequestURL().toString().contains("/swagger") || httpServletRequest.getRequestURL().toString().contains("/api-docs")) {
                 filterChain.doFilter(servletRequest, servletResponse);
                 return;
