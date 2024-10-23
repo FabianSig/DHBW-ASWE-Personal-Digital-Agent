8d9dc9c22cc4bdebf5216b5745b65d902f9fdcdd
Aziz Carducci <carducciaziz@gmail.com>
2024-10-23 16:46:51 +0200

    feat: add weather service

---
+++ Microservice/Wetter/src/main/java/com/example/dhbwaswepersonaldigitalagentmswetter/service/WetterService.java
@@ -0,0 +1,12 @@
+package com.example.dhbwaswepersonaldigitalagentmswetter.service;
+
+
+import java.util.Optional;
+
+public class WetterService {
+
+    public Wetter getWetter(Optional<String> datum) {
+        return null;
+    }
+
+}
