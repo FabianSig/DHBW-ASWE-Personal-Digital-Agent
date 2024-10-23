e357b177fc642d97caf936b0cb3cb8977ffce6d5
fabiansig <fabian.sigmund1@gmail.com>
2024-10-23 15:47:16 +0200

    Revert "add: logging"
    This reverts commit cac774e54f84f6db25779d727dff8331109649d3.

--- Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/controller/ChatGPTController.java
+++ Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/controller/ChatGPTController.java
@@ -6,8 +6,6 @@ import nikomitk.mschatgpt.dto.ChatGPTAudioResponse;
 import nikomitk.mschatgpt.dto.ChatGPTResponseChoice;
 import nikomitk.mschatgpt.dto.Request;
 import nikomitk.mschatgpt.service.ChatGPTService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,35 +16,18 @@ import org.springframework.web.multipart.MultipartFile;
 @CrossOrigin(origins = "*")
 public class ChatGPTController {
 
-    private static final Logger logger = LoggerFactory.getLogger(ChatGPTController.class);
     private final ChatGPTService chatGPTService;
 
     @PostMapping("/message")
     @ResponseStatus(HttpStatus.OK)
     public ChatGPTResponseChoice sendMessage(@RequestBody Request request) {
-        logger.info("Received message request: {}", request);
-        try {
-            ChatGPTResponseChoice response = chatGPTService.sendMessage(request);
-            logger.info("Message response: {}", response);
-            return response;
-        } catch (Exception e) {
-            logger.error("Error while processing message request: {}", request, e);
-            throw e;
-        }
+        return chatGPTService.sendMessage(request);
     }
 
     @PostMapping("/audio")
     @ResponseStatus(HttpStatus.OK)
     public ChatGPTAudioResponse sendAudio(@RequestPart("file") MultipartFile file) {
-        logger.info("Received audio file: {}", file.getOriginalFilename());
         ChatGPTAudioRequest audioRequest = new ChatGPTAudioRequest(file, "whisper-1", "de");
-        try {
-            ChatGPTAudioResponse response = chatGPTService.sendAudio(audioRequest);
-            logger.info("Audio response: {}", response);
-            return response;
-        } catch (Exception e) {
-            logger.error("Error while processing audio request for file: {}", file.getOriginalFilename(), e);
-            throw e;
-        }
+        return chatGPTService.sendAudio(audioRequest);
     }
 }
