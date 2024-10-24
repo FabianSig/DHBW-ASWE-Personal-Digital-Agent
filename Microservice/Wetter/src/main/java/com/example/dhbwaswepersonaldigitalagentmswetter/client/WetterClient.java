2e1ca6a36fcccdfeb41f0294d949e1315b0317c1
Aziz Carducci <125400037+A2i2C@users.noreply.github.com>
2024-10-24 13:40:54 +0200

    refactor: delete APIkey

--- Microservice/Wetter/src/main/java/com/example/dhbwaswepersonaldigitalagentmswetter/client/WetterClient.java
+++ Microservice/Wetter/src/main/java/com/example/dhbwaswepersonaldigitalagentmswetter/client/WetterClient.java
@@ -5,6 +5,6 @@ import org.springframework.web.service.annotation.PostExchange;
 
 public interface WetterClient extends RestClient {
 
-        @PostExchange("/data/2.5/weather?q=Stuttgart&units=metric&appid=57be2938eb617720df17785c11e864d9")
+        @PostExchange("/data/2.5/weather?q=Stuttgart&units=metric&appid=)
         String getWetter();
 }
