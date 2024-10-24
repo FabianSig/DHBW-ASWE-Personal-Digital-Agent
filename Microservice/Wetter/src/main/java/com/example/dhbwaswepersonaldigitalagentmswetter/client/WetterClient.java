f6c7eccfa3e252abd88aa89eaf2d87675949c5bd
Aziz Carducci <carducciaziz@gmail.com>
2024-10-24 09:56:54 +0200

    feat: create Weather API

--- Microservice/Wetter/src/main/java/com/example/dhbwaswepersonaldigitalagentmswetter/client/WetterClient.java
+++ Microservice/Wetter/src/main/java/com/example/dhbwaswepersonaldigitalagentmswetter/client/WetterClient.java
@@ -3,10 +3,11 @@ package com.example.dhbwaswepersonaldigitalagentmswetter.client;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
 public interface WetterClient extends RestClient {
 
-        @PostExchange("https://api.openweathermap.org/data/2.5/forecast/daily?lat=48.78232&lon=9.17702&cnt=3&appid=DEIN_API_KEY&units=metric&lang=de\n")
-        String getWetter(@RequestBody MultiValueMap<String, String> formData);
+        @PostExchange("/data/2.5/weather?q=Stuttgart&appid=system.getenv('API_KEY')")
+        String getWetter();
 }
--- Microservice/Wetter/src/main/java/com/example/dhbwaswepersonaldigitalagentmswetter/service/WetterService.java
+++ Microservice/Wetter/src/main/java/com/example/dhbwaswepersonaldigitalagentmswetter/service/WetterService.java
@@ -1,13 +1,31 @@
 package com.example.dhbwaswepersonaldigitalagentmswetter.service;
 
-
 import com.example.dhbwaswepersonaldigitalagentmswetter.dto.Wetter;
+import com.example.dhbwaswepersonaldigitalagentmswetter.client.WetterClient;
+import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 import java.util.Optional;
 
+@Service
+@Slf4j
 public class WetterService {
 
-    public Wetter getWetter(Optional<String> datum) {
+    private final WetterClient wetterClient;
+
+    public WetterService(WetterClient wetterClient) {
+        this.wetterClient = wetterClient;
+    }
+
+    public Wetter getWetter(Optional<String> datumParam) {
+        String datum = datumParam.orElse(LocalDate.now().toString());
+
+        String startThisWeek = LocalDate.now().getDayOfWeek().getValue() <= 3 ? LocalDate.now().minusDays(LocalDate.now().getDayOfWeek().getValue()).toString() : LocalDate.now().plusDays(7 - LocalDate.now().getDayOfWeek().getValue()).toString();
+        String startNextWeek = LocalDate.now().getDayOfWeek().getValue() <= 3 ? LocalDate.now().plusDays(7L - LocalDate.now().getDayOfWeek().getValue()).toString() : LocalDate.now().plusDays(14 - LocalDate.now().getDayOfWeek().getValue()).toString();
+
+        String websiteHtml = wetterClient.getWetter();
         return null;
     }
 
