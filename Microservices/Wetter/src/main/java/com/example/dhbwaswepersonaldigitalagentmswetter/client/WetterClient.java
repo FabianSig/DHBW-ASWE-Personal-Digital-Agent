4e91ad1bd2bd9f51a7cdb7a93f4cd33baa8eb883
Aziz Carducci <carducciaziz@gmail.com>
2024-10-24 13:17:58 +0200

    refactor: delete unecessary imports

--- Microservices/Wetter/src/main/java/com/example/dhbwaswepersonaldigitalagentmswetter/client/WetterClient.java
+++ Microservices/Wetter/src/main/java/com/example/dhbwaswepersonaldigitalagentmswetter/client/WetterClient.java
@@ -1,13 +1,10 @@
 package com.example.dhbwaswepersonaldigitalagentmswetter.client;
 
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
 public interface WetterClient extends RestClient {
 
-        @PostExchange("/data/2.5/weather?q=Stuttgart&appid=system.getenv('API_KEY')")
+        @PostExchange("/data/2.5/weather?q=Stuttgart&units=metric&appid=57be2938eb617720df17785c11e864d9")
         String getWetter();
 }
