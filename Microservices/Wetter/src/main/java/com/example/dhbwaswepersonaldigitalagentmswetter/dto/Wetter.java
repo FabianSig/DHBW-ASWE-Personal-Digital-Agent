52db37fb0a948bc6bba5afcf3cc8908746446ef5
Aziz Carducci <carducciaziz@gmail.com>
2024-10-24 13:38:50 +0200

    feat: add more variables

--- Microservices/Wetter/src/main/java/com/example/dhbwaswepersonaldigitalagentmswetter/dto/Wetter.java
+++ Microservices/Wetter/src/main/java/com/example/dhbwaswepersonaldigitalagentmswetter/dto/Wetter.java
@@ -3,10 +3,10 @@ package com.example.dhbwaswepersonaldigitalagentmswetter.dto;
 import java.util.List;
 
 public record Wetter(
-        List<Double> temperatur,
-        List<String> wetterBeschreibung,
-        List<Double> windgeschwindigkeit,
-        List<Integer> luftfeuchtigkeit,
-        List<Double> niederschlagswahrscheinlichkeit,
-        List<String> datum
+        double temp,
+        double feels_like,
+        double temp_min,
+        double temp_max,
+        String country
 ) {}
+
