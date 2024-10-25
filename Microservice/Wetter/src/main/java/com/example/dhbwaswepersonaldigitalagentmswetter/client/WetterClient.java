cca7760d5d709285ade035ec46dea97a57ef5f25
Aziz Carducci <carducciaziz@gmail.com>
2024-10-25 13:04:45 +0200

    refactor: api called through systemenv

--- Microservice/Wetter/src/main/java/com/example/dhbwaswepersonaldigitalagentmswetter/client/WetterClient.java
+++ Microservice/Wetter/src/main/java/com/example/dhbwaswepersonaldigitalagentmswetter/client/WetterClient.java
@@ -6,6 +6,6 @@ import org.springframework.web.service.annotation.PostExchange;
 
 public interface WetterClient extends RestClient {
 
-        @PostExchange("/data/2.5/weather?q=Stuttgart&units=metric&appid=")
+        @PostExchange("/data/2.5/weather?q=Stuttgart&units=metric&appid=system.getenv('API_KEY')")
         Wetter getWetter();
 }
