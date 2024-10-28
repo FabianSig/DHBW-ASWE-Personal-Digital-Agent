b35a16e0703c57d9dc83051cb7b65148681fccfa
Sven Sendke <svensendke@gmail.com>
2024-10-28 10:14:08 +0100

    feat: add chat

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -4,33 +4,24 @@ import {SearchBarComponent} from './search-bar/search-bar.component';
 import {MenuComponent} from './menu/menu.component';
 import {MenuResponse} from './interfaces/menu-response';
 import {AudioRecorderComponent} from './audio-recorder/audio-recorder.component';
-import {ChatGPTResponse} from './interfaces/chat-gptresponse';
-import {AudioResponse} from './interfaces/audio-response';
 import {AlarmClockComponent} from './alarm-clock/alarm-clock.component';
 import {PreferencesComponent} from './preferences/preferences.component';
 import {HeaderComponent} from './header/header.component';
+import {ChatComponent} from './chat/chat.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SearchBarComponent, MenuComponent, AudioRecorderComponent, AlarmClockComponent, PreferencesComponent, HeaderComponent],
+  imports: [RouterOutlet, SearchBarComponent, MenuComponent, AudioRecorderComponent, AlarmClockComponent, PreferencesComponent, HeaderComponent, ChatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  chatGPTResponse?: ChatGPTResponse;
   menuResponse?: MenuResponse;
-  audioResponse?: AudioResponse;
-
-  onChatGPTResponse(response: ChatGPTResponse): void {
-    this.chatGPTResponse = response;
-  }
 
   onMenuResponse(response: MenuResponse): void {
     this.menuResponse = response;
   }
 
-  onAudioResponse(response: AudioResponse): void {
-    this.audioResponse = response;
-  }
+
 }
