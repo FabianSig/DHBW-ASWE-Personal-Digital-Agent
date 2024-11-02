47d8761f0f998cff3a0f38c954aa6dc7fdc0f00f
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-11-02 11:59:07 +0100

    add: morning route

--- Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/repository/PromptRepository.java
+++ Microservices/Chatgpt/src/main/java/nikomitk/mschatgpt/repository/PromptRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface PromptRepository extends MongoRepository<Prompt, String> {
     List<Prompt> findByPromptId(String promptId);
 
+    Prompt findFirstByPromptId(String promptId);
 }
