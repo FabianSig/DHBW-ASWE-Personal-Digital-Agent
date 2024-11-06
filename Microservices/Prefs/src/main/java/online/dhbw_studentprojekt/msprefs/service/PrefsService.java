824348b48ba6683deddfd805f1723c06432f8fe1
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-11-06 15:32:38 +0100

    fix: return optional instead of throwing error

--- Microservices/Prefs/src/main/java/online/dhbw_studentprojekt/msprefs/service/PrefsService.java
+++ Microservices/Prefs/src/main/java/online/dhbw_studentprojekt/msprefs/service/PrefsService.java
@@ -3,9 +3,9 @@ package online.dhbw_studentprojekt.msprefs.service;
 import lombok.RequiredArgsConstructor;
 import online.dhbw_studentprojekt.dto.prefs.Preference;
 import online.dhbw_studentprojekt.msprefs.repository.PrefsRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,17 +13,20 @@ public class PrefsService {
 
     private final PrefsRepository prefsRepository;
 
-    public Preference getPref(String id) {
-        return prefsRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)).toDto();
+    public Optional<online.dhbw_studentprojekt.msprefs.model.Preference> getPref(String id) {
+
+        return prefsRepository.findById(id);
     }
 
 
     public void createPref(Preference preference) {
+
         online.dhbw_studentprojekt.msprefs.model.Preference pref = new online.dhbw_studentprojekt.msprefs.model.Preference(preference.id(), preference.value());
         prefsRepository.save(pref);
     }
 
     public void deletePref(String id) {
+
         prefsRepository.deleteById(id);
     }
 
