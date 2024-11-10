344729b004adc2a9cb2ada6a4ca52c0021e71228
mohammad.alomari <alomari.mohammad2001@gmail.com>
2024-11-10 18:19:22 +0100

    Removed ActiveProfiles annotation

--- Microservices/Stock/src/test/java/online/dhbw_studentprojekt/msstock/controller/StockControllerTest.java
+++ Microservices/Stock/src/test/java/online/dhbw_studentprojekt/msstock/controller/StockControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -25,7 +24,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(StockController.class)
 @ExtendWith(MockitoExtension.class)
-@ActiveProfiles("test")
 @Import(TestSecurityConfig.class)
 public class StockControllerTest {
     @MockBean
