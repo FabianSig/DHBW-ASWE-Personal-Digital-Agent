77d97e9be749cfc78596e5de9ec350d2ba304042
mohammad.alomari <alomari.mohammad2001@gmail.com>
2024-11-10 23:08:58 +0100

    Added getNews with no count

--- Microservices/News/src/test/java/online/dhbw_studentprojekt/msnews/service/NewsServiceTest.java
+++ Microservices/News/src/test/java/online/dhbw_studentprojekt/msnews/service/NewsServiceTest.java
@@ -21,6 +21,26 @@ class NewsServiceTest {
 
     @InjectMocks
     private NewsService newsService;
+    @Test
+    void getNews_noCount() {
+        // Arrange
+        Article.Source source = new Article.Source("1", "Test Source");
+        Article article1 = new Article(source, "Author1", "Title1", "Description1", "http://url1.com", "http://urlToImage1.com", "2022-01-01T12:00:00Z");
+        Article article2 = new Article(source, "Author2", "Title2", "Description2", "http://url2.com", "http://urlToImage2.com", "2022-01-01T12:00:00Z");
+        List<Article> articles = List.of(article1, article2);
+
+        News news = new News("ok", 2, articles);
+        when(newsClient.getNews("sports")).thenReturn(news);
+
+        // Act
+        List<Article> result = newsService.getNews("sports", Optional.empty());
+
+        // Assert
+        assertThat(result).hasSize(2);
+        assertThat(result.get(0).title()).isEqualTo("Title1");
+        assertThat(result.get(1).title()).isEqualTo("Title2");
+        verify(newsClient, times(1)).getNews("sports");
+    }
 
     @Test
     void getNews_withCount() {
