665de9658494573b51ecd2d45157e9b1e56219e6
mohammad.alomari <alomari.mohammad2001@gmail.com>
2024-11-10 17:50:57 +0100

    Added TestSecurityConfig to bypass the Security filter in Tests only

---
+++ Microservices/Stock/src/test/java/online/dhbw_studentprojekt/msstock/config/TestSecurityConfig.java
@@ -0,0 +1,23 @@
+package online.dhbw_studentprojekt.msstock.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class TestSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        // Disable security for test profile
+
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
+                        .permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
