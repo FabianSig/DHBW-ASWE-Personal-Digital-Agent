582ac4f87d95ca993dfbed3565cb34a557cbf359
Aziz Carducci <carducciaziz@gmail.com>
2024-11-15 18:34:23 +0100

    delete: unnessecary Env data

--- Frontend/src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrlLogic: '/api/logic/message',
-  apiUrlChatgpt: '/api/chatgpt/intention',
-  apiUrlSpeisekarte: '/api/speisekarte',
-  apiUrlAudio: '/api/chatgpt/audio',
-  apiUrlPrefs: '/api/prefs',
-  apiUrlTrigger: '/api/logic/trigger'
-};
