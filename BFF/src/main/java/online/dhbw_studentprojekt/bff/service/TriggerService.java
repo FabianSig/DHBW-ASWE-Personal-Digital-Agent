9eca084e65e59fd099f71ade4bd4f1223eae7cbc
Fabian <fabian.sigmund1@gmail.com>
2024-11-15 13:57:05 +0100

    fix: iso default date morning

--- BFF/src/main/java/online/dhbw_studentprojekt/bff/service/TriggerService.java
+++ BFF/src/main/java/online/dhbw_studentprojekt/bff/service/TriggerService.java
@@ -8,6 +8,8 @@ import online.dhbw_studentprojekt.dto.trigger.Trigger;
 import online.dhbw_studentprojekt.dto.trigger.TriggerResponse;
 import org.springframework.stereotype.Service;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,7 +25,13 @@ public class TriggerService {
         List<Trigger> triggers = new ArrayList<>();
 
         RaplaResponse raplaResponse = raplaClient.getLectureTimes(date);
-        String triggerWecker = prefsClient.getPreference("wecker-" + date).map(pref -> pref.value().getFirst()).orElse("08:00");
+        String triggerWecker = prefsClient.getPreference("wecker-" + date).map(pref -> pref.value()
+                                            .getFirst())
+                                            .orElseGet(() -> {
+                                                LocalDateTime defaultDateTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(8, 0));
+                                                ZonedDateTime zonedDateTime = defaultDateTime.atZone(ZoneId.of("Europe/Berlin"));
+                                                return zonedDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+                                            });
 
         triggers.add(new Trigger("/api/logic/morning", triggerWecker));
         triggers.add(new Trigger("/api/logic/mittag", raplaResponse.end_of_first_lecture()));
