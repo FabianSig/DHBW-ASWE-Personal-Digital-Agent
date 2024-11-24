5d39b5bf26aff111fb373853d43e47e25d4ec0a4
Sven Sendke <102304412+einBuegelbrett@users.noreply.github.com>
2024-11-24 15:10:15 +0100

    feat: add functioning tts (#116)
    * feat: add functioning tts
    
    * fix: remove ttsservice

--- Frontend/src/app/chat-input/search-bar.component.ts
+++ Frontend/src/app/chat-input/search-bar.component.ts
@@ -31,6 +31,30 @@ export class SearchBarComponent {
     this.chatService.addMessage(this.searchTerm, 'user');
     this.apiService.getChatGPTData(this.searchTerm).subscribe(response => {
       this.chatService.addMessage(response, 'chatgpt');
+
+      // Text-To-Speech
+      this.apiService.getTtsAudioFile(response).subscribe({
+        next: (res) => {
+          // Create an object URL from the Blob
+          const audioUrl = URL.createObjectURL(res);
+
+          // Create an Audio object and set its source to the Blob URL
+          const audio = new Audio(audioUrl);
+
+          // Play the audio
+          audio.play().catch((error) => {
+            console.error('Error playing audio:', error);
+          });
+
+          // Revoke the URL after some time to free memory
+          audio.onended = () => {
+            URL.revokeObjectURL(audioUrl);
+          };
+        },
+        error: (error) => {
+          console.error('Error occurred:', error);
+        }
+      });
     })
     this.searchTerm = '';
   }
