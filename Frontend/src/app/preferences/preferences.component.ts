f652ada088501dcf1029d5e6de1efbeef46330e8
Fabian <fabian.sigmund1@gmail.com>
2024-11-26 15:02:15 +0100

    Revert "add: save preferences in one json obj in DB"
    This reverts commit 83629d3c2a8448f739307cc041dce437c9e53eb0.

--- Frontend/src/app/preferences/preferences.component.ts
+++ Frontend/src/app/preferences/preferences.component.ts
@@ -70,26 +70,50 @@ export class PreferencesComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log("ngOnInit")
-
-    this.apiService.getPreferenceFormFromDB().subscribe((form: any) => {
-      console.log("form:", form);
-
-      if (form) {
-        console.log('Loaded Preferences:', form);
-
-        if (!form.transportation?.mode) {
-          form.transportation = { ...form.transportation, mode: 'transit' };
-        }
-
-        this.preferencesForm.setValue(form);
+    // Load existing preferences from localStorag
+    const savedPreferences = localStorage.getItem('preferences');
+    if (savedPreferences) {
+      const parsedPreferences = JSON.parse(savedPreferences);
+      console.log('Loaded Preferences:', parsedPreferences);
+      if (!parsedPreferences.transportation?.mode) {
+        parsedPreferences.transportation = { ...parsedPreferences.transportation, mode: 'transit' };
       }
-    });
+      this.preferencesForm.setValue(parsedPreferences);
+    }
   }
 
   onSubmit() {
     const formData = this.preferencesForm.value;
-    console.log("formData: " + JSON.stringify(formData))
-    this.apiService.setPreferenceFormInDB(formData).subscribe(() => {})
+    this.preferencesJson = JSON.stringify(formData);
+
+    // Save the preferences in localStorage
+    localStorage.setItem('preferences', this.preferencesJson);
+
+    // set alarm
+    const alarmId = "wecker-" + formData.alarm.alarmDate;
+    const alarmValue = formData.alarm.alarmDate + "T" + formData.alarm.alarmTime + ":00"  ;
+    const alarm$ = this.apiService.setAlarmPreference(alarmId, alarmValue);
+
+    const travelMode$ = this.apiService.setTravelModePreference(formData.transportation.mode);
+
+    const allergens = Object.keys(formData.allergens).filter(key => formData.allergens[key]);
+    const allergens$ = this.apiService.setAllergenePreference(allergens);
+
+    const workAddress$ = this.apiService.setWorkAddress(formData.workAddress.address)
+
+    const homeAddress$ = this.apiService.setHomeAddress(formData.homeAddress.address)
+
+    //Wir machen hier mit RxJS einen forkjoin, damit wir sicherstellen, dass alle preferences gesezt worden sind bevor wir reloaded
+    forkJoin([alarm$, travelMode$, allergens$, workAddress$, homeAddress$]).subscribe({
+      next: ([alarmResult, travelModeResult, allergensResult]) => {
+        console.log("All API calls completed.");
+        this.triggerService.reload();
+      },
+      error: (err) => {
+        console.error("Error during API calls:", err);
+      }
+    });
+
+
   }
 }
--- Microservices/Prefs/src/main/java/online/dhbw_studentprojekt/msprefs/controller/PrefsController.java
+++ Microservices/Prefs/src/main/java/online/dhbw_studentprojekt/msprefs/controller/PrefsController.java
@@ -2,16 +2,13 @@ package online.dhbw_studentprojekt.msprefs.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import online.dhbw_studentprojekt.dto.prefs.Preference;
-import online.dhbw_studentprojekt.msprefs.model.PreferenceForm;
 import online.dhbw_studentprojekt.msprefs.service.PrefsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@Slf4j
 @RestController
 @RequestMapping("/api/prefs")
 @RequiredArgsConstructor
@@ -42,22 +39,5 @@ public class PrefsController {
 
         prefsService.deletePref(id);
     }
-    @GetMapping("/all")
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Get all preferences.")
-    public PreferenceForm getAllPrefs() {
-
-        return prefsService.getAllPrefs();
-    }
-
-    @PostMapping("/all")
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Set all preferences.")
-    public void setAllPrefs(@RequestBody String preferenceFormJSON) {
-        System.out.println("Received JSON: " + preferenceFormJSON);
-        log.info("Setting all preferences");
-        prefsService.setAllPrefs(preferenceFormJSON);
-    }
-
 
 }
