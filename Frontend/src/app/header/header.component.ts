cae039c98947e3e6be7845a6d42dbcf9e412bc1a
Sven Sendke <svensendke@gmail.com>
2024-11-26 14:54:59 +0100

    feat: comments for header component

--- Frontend/src/app/header/header.component.ts
+++ Frontend/src/app/header/header.component.ts
@@ -16,18 +16,21 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.updateDateTime();
+    // Set up a timer to update the date and time every second in the header
     this.timerId = setInterval(() => {
       this.updateDateTime();
     }, 1000);
   }
 
   ngOnDestroy() {
+    // Clear the timer to prevent memory leaks when the component is destroyed
     if (this.timerId) {
       clearInterval(this.timerId);
     }
   }
 
   private updateDateTime() {
+    // Updates day and time for the header
     const dateTime = new Date();
     const day = String(dateTime.getDate()).padStart(2, '0');
     const month = String(dateTime.getMonth() + 1).padStart(2, '0'); // +1, because months fo from 0 to 11
@@ -36,5 +39,4 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.day = `${day}.${month}.${year}`;
     this.time = dateTime.toTimeString().split(' ')[0];
   }
-
 }
