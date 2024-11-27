f1c66ac51d884ccec50fa6086239235e8309643d
Aziz Carducci <carducciaziz@gmail.com>
2024-11-27 11:09:53 +0100

    refactor: trigger

--- Frontend/src/app/services/trigger.service.spec.ts
+++ Frontend/src/app/services/trigger.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { TriggerService } from './trigger.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {ApiService} from './api.service';
+import {of, throwError} from 'rxjs';
 
 
 describe('TriggerService', () => {
@@ -49,5 +50,32 @@ describe('TriggerService', () => {
 
     expect(clearTimeout).toHaveBeenCalledTimes(2); // Verify that clearTimeout was called for each timeout
     expect(service['timoutReferenceMap']).toEqual({}); // Ensure the map is cleared
+  });
+
+  it('should process triggers correctly', () => {
+    const mockTriggerData = {
+      triggers: [
+        { route: '/routine1', time: new Date().getTime() + 5000 }, // 5 seconds in the future
+        { route: '/routine2', time: new Date().getTime() + 10000 } // 10 seconds in the future
+      ]
+    };
+
+    spyOn(apiService, 'getTriggerData').and.returnValue(of(mockTriggerData));
+    spyOn(apiService, 'executeCustomTriggerRoutine').and.returnValue(of('Routine executed'));
+    // @ts-ignore
+    spyOn(service, 'handleTriggerDisplayText').and.callThrough();
+
+    service.setOffTrigger();
+
+    expect(apiService.getTriggerData).toHaveBeenCalled();
+    expect(service['triggerMap']['/routine1']).toBeDefined();
+    expect(service['triggerMap']['/routine2']).toBeDefined();
+
+    setTimeout(() => {
+      expect(apiService.executeCustomTriggerRoutine).toHaveBeenCalledWith('/routine1');
+      expect(apiService.executeCustomTriggerRoutine).toHaveBeenCalledWith('/routine2')
+      // @ts-ignore
+      expect(service.handleTriggerDisplayText).toHaveBeenCalled();
+    }, 11000); // Wait for the longest timeout
   });
 });
